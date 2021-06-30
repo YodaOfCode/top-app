330004dd730f9f9f3cae3fb45d7b6b9e7f7727bf
ac88 <mmaxpokerr@gmail.com>
2021-06-30 19:42:39 +0300

    top-app version - 0.1.

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Htag from "../components/Htag/Htag";
+
+export default function Home(): JSX.Element {
+    return (
+        <>
+            <Htag tag='h1'>Text1</Htag>
+            <Htag tag='h2'>Text2</Htag>
+            <Htag tag='h3'>Text3</Htag>
+        </>
+    );
+}
